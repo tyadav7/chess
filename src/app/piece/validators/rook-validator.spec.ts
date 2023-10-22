a306bd3178ab1ef923e7173c58749786a72c80e2
tushary <tushar.yadav@amdocs.com>
2023-10-22 07:07:25 +0530

    modifying rook validator spec to us ng-mocks

--- src/app/piece/validators/rook-validator.spec.ts
+++ src/app/piece/validators/rook-validator.spec.ts
@@ -1,55 +1,19 @@
 import { TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
 import { IPoint } from 'src/app/board/cell/cell.component';
-import { IBoardService } from 'src/app/board/i-board-service';
-import { IPlayerService } from 'src/app/player/i-player.service';
 import { IObstructionValidator } from '../obstructors/i-obstruction-validator';
-import { RookValidator } from './rook-validator';
 import { Rook } from '../pieces/rook';
+import { RookValidator } from './rook-validator';
+import { MockService } from 'ng-mocks';
+import { BoardService } from 'src/app/board/board.service';
+import { ObstructionValidatorService } from '../obstructors/obstruction.service';
+import { Player } from 'src/app/player/player';
+import { PlayerService } from 'src/app/player/player.service';
 
-const mockObstructionService: IObstructionValidator = {
-	isObstructed: function (from: IPoint, to: IPoint): boolean {
-		throw new Error('Function not implemented.');
-	},
-	checkIfSpotIsOccupied: function (to: IPoint): boolean {
-		throw new Error('Function not implemented.');
-	}
-};
-
-const mockBoardService: IBoardService = {
-	view: [[]],
-	pick: function (position: IPoint): void {
-		throw new Error('Function not implemented.');
-	},
-	drop: function (position: IPoint): void {
-		throw new Error('Function not implemented.');
-	},
-	resetMove: function (): void {
-		throw new Error('Function not implemented.');
-	},
-	resetMove$: of(false),
-}
-
-const mockPlayerService: IPlayerService = {
-	players: [],
-	nextPlayer: function (): void {
-		throw new Error('Function not implemented.');
-	},
-	player1: {
-		name: 'player1',
-		color: 'white'
-	},
-	player2: {
-		name: 'player2',
-		color: 'black'
-	},
-	currentPlayer: {
-		name: 'player1',
-		color: 'white'
-	}
-}
-
-mockPlayerService.currentPlayer = mockPlayerService.player1;
+const mockObstructionService: IObstructionValidator = MockService(ObstructionValidatorService);
+const mockBoardService = MockService(BoardService);
+const mockPlayerService = MockService(PlayerService);
+const player1 = new Player('white', 'player1');
+const player2 = new Player('black', 'player2');
 
 describe('RookValidator', () => {
 	let rookValidator: RookValidator;
@@ -63,6 +27,9 @@ describe('RookValidator', () => {
         });
 
 		rookValidator = TestBed.inject(RookValidator);
+        spyOnProperty(mockPlayerService, 'player1', 'get').and.returnValue(player1);
+        spyOnProperty(mockPlayerService, 'player2', 'get').and.returnValue(player2);
+        mockPlayerService.currentPlayer = mockPlayerService.player1;
 	});
 
 	it('should be created', () => {
@@ -85,7 +52,7 @@ describe('RookValidator', () => {
     it('should allow a move to a spot occupied by an enemy', () => {
         let from: IPoint = { x: 0, y: 0 };
         let to: IPoint = { x: 5, y: 0 };
-        mockBoardService.view = [[new Rook(from, mockPlayerService.player1)],[],[],[],[],[new Rook(to, mockPlayerService.player2)]];
+        spyOnProperty(mockBoardService, 'view', 'get').and.returnValue([[new Rook(from, mockPlayerService.player1)],[],[],[],[],[new Rook(to, mockPlayerService.player2)]]);
         spyOn(mockObstructionService, 'isObstructed').and.returnValue(false);
         spyOn(mockObstructionService, 'checkIfSpotIsOccupied').and.returnValue(true);
         let result = rookValidator.validateMove(from, to);
@@ -95,7 +62,7 @@ describe('RookValidator', () => {
     it('should not allow a move to a spot occupied by an ally', () => {
         let from: IPoint = { x: 0, y: 0 };
         let to: IPoint = { x: 5, y: 0 };
-        mockBoardService.view = [[new Rook(from, mockPlayerService.player1)],[],[],[],[],[new Rook(to, mockPlayerService.player1)]];
+        spyOnProperty(mockBoardService, 'view', 'get').and.returnValue([[new Rook(from, mockPlayerService.player1)],[],[],[],[],[new Rook(to, mockPlayerService.player1)]]);
         spyOn(mockObstructionService, 'isObstructed').and.returnValue(false);
         spyOn(mockObstructionService, 'checkIfSpotIsOccupied').and.returnValue(true);
         let result = rookValidator.validateMove(from, to);
@@ -114,7 +81,7 @@ describe('RookValidator', () => {
     it('should not allow a move to a spot that is obstructed', () => {
         let from: IPoint = { x: 0, y: 0 };
         let to: IPoint = { x: 5, y: 0 };
-        mockBoardService.view = [[new Rook(from, mockPlayerService.player1)],[],[],[],[new Rook( { x: 4 , y: 0 }, mockPlayerService.player1)],[]];
+        spyOnProperty(mockBoardService, 'view', 'get').and.returnValue([[new Rook(from, mockPlayerService.player1)],[],[],[],[new Rook( { x: 4 , y: 0 }, mockPlayerService.player1)],[]]);
         spyOn(mockObstructionService, 'isObstructed').and.returnValue(true);
         spyOn(mockObstructionService, 'checkIfSpotIsOccupied').and.returnValue(false);
         let result = rookValidator.validateMove(from, to);
@@ -124,7 +91,7 @@ describe('RookValidator', () => {
     it('should not allow to capture a piece that is obstructed', () => {
         let from: IPoint = { x: 0, y: 0 };
         let to: IPoint = { x: 5, y: 0 };
-        mockBoardService.view = [[new Rook(from, mockPlayerService.player1)],[],[new Rook( { x: 2 , y: 0 }, mockPlayerService.player1)],[],[],[new Rook( { x: 5 , y: 0 }, mockPlayerService.player2)]];
+        spyOnProperty(mockBoardService, 'view', 'get').and.returnValue([[new Rook(from, mockPlayerService.player1)],[],[new Rook( { x: 2 , y: 0 }, mockPlayerService.player1)],[],[],[new Rook( { x: 5 , y: 0 }, mockPlayerService.player2)]]);
         spyOn(mockObstructionService, 'isObstructed').and.returnValue(true);
         spyOn(mockObstructionService, 'checkIfSpotIsOccupied').and.returnValue(false);
         let result = rookValidator.validateMove(from, to);
